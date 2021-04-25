16b018233a12f44358ecce214f37f28f59460438
ravigne <52163304+ravigne@users.noreply.github.com>
2021-04-25 19:59:37 +0530

    Update Utils.java

--- app/src/main/java/com/mrsy/help/Utils.java
+++ app/src/main/java/com/mrsy/help/Utils.java
@@ -3,8 +3,8 @@ package com.mrsy.help;
 public class Utils {
 
     //This is your from email
-    public static final String EMAIL = "ravignelyf@gmail.com";
+    public static final String EMAIL = "Your email";
 
     //This is your from email password
-    public static final String PASSWORD = "9031643430";
+    public static final String PASSWORD = "your pass";
 }
